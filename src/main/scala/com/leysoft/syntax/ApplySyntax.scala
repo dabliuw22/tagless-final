dd9ab14a0ae1021ec7a7417b850bf0d64333681f
Will Leyton <wleylop22@gmail.com>
2020-12-10 08:47:55 -0500

    Add apply syntax

---
+++ src/main/scala/com/leysoft/syntax/ApplySyntax.scala
@@ -0,0 +1,17 @@
+package com.leysoft.syntax
+
+import com.leysoft.algebra.Apply
+
+private[syntax] trait ApplySyntax {
+
+  implicit final class ToApplySyntax[F[_], A](val fa: F[A]) {
+    def ap[B](ff: F[A => B])(implicit F: Apply[F]): F[B] =
+      F.ap(ff)(fa)
+
+    def <*>[B](ff: F[A => B])(implicit F: Apply[F]): F[B] =
+      fa.ap(ff)
+
+    def *>[B](fa: F[A])(fb: F[B])(implicit F: Apply[F]): F[B] =
+      F.productR(fa)(fb)
+  }
+}
